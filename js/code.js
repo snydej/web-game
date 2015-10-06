d7a915cd2aae5b12a556aef820219b4fe82e22fb
Josh Snyder <josh@qumulo.com>
2015-10-06 01:04:14 -0700

    Added some terrain generation.

--- js/code.js
+++ js/code.js
@@ -2,21 +2,26 @@
 
 var canvas = document.getElementById('canvas');
 
+var land = generate(20, 20);
+
 function fillRect(ctx, view, x, y, w, h) {
     ctx.fillRect(view.transX(x), view.transY(y),
             view.transWidth(w), view.transHeight(h));
 }
 
-function render(canvas) {
+function render(canvas, land) {
+    var landHeight = land.length;
+    var landWidth = land[0].length;
+    var canvasView = new view(0, landWidth, 0, landHeight,
+            canvas.width, canvas.height);
+
     var ctx = canvas.getContext("2d");
-    var canvasView = new view(0, 1, 0, 1, 100, 100);
-
-    ctx.fillStyle = "rgb(255,0,0)";
-    fillRect(ctx, canvasView, 0, 0, 1, 1);
-
-    ctx.fillStyle = "rgb(0, 0, 255)";
-    fillRect(ctx, canvasView, 0.5, 0, 0.5, 0.5);
-    fillRect(ctx, canvasView, 0, 0.5, 0.5, 0.5);
+    for (var y = 0; y < landHeight; y++) {
+        for (var x = 0; x < landWidth; x++) {
+            ctx.fillStyle = land[y][x].color;
+            fillRect(ctx, canvasView, x, y, 1, 1);
+        }
+    }
 }
 
-render(canvas);
+render(canvas, land);
---
+++ js/world.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var TERRAIN = {
+    GRASS: {
+        color: "rgb(0,255,0)",
+        weight: 40
+    },
+
+    TREE:  {
+        color: "rgb(0,128,0)",
+        weight: 2
+    },
+
+    WATER: {
+        color: "rgb(0,0,255)",
+        weight: 3
+    }
+};
+
+function generate(w, h) {
+    var totalWeight = 0;
+    var type;
+
+    for (type in TERRAIN) {
+        totalWeight += TERRAIN[type].weight;
+    }
+
+    var land = [];
+    for (var r = 0; r < h; r++) {
+        var landRow = [];
+        for (var c = 0; c < w; c++) {
+            var randomChoice = totalWeight * Math.random();
+            for (type in TERRAIN) {
+                randomChoice -= TERRAIN[type].weight;
+                if (randomChoice < 0) {
+                    break;
+                }
+            }
+            landRow.push(TERRAIN[type]);
+        }
+        land.push(landRow);
+    }
+
+    return land;
+}
