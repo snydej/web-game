1537c3f6b719e719de7f33f400c1cfcb21c52962
Josh Snyder <joshsnyder@msn.com>
2015-10-12 18:41:46 -0700

    Change how enums are created.

--- js/enum.js
+++ js/enum.js
@@ -10,7 +10,8 @@ EnumValue.prototype.toString = function() {
     return this.name;
 }
 
-function Enum(enumSpec) {
-    for (var enumValue in enumSpec)
-        this[enumValue] = new EnumValue(enumValue, enumSpec[enumValue], this);
+function Enum() {
+    var numValues = arguments.length;
+    for (var i = 0; i < numValues; i++)
+        this[arguments[i]] = new EnumValue(arguments[i], i, this);
 }
--- test/js/enum-test.js
+++ test/js/enum-test.js
@@ -4,20 +4,20 @@
 QUnit.module('enum');
 
 QUnit.test('enum has specified elements', function(assert) {
-    var simpleEnum = new Enum({A: 0, B: 1});
+    var simpleEnum = new Enum("A", "B");;
     assert.ok(simpleEnum.A);
     assert.ok(simpleEnum.B);
     assert.notOk(simpleEnum.C);
 });
 
 QUnit.test('enum values are distict', function(assert) {
-    var simpleEnum = new Enum({A: 0, B: 1});
+    var simpleEnum = new Enum("A", "B");;
     assert.equal(simpleEnum.A, simpleEnum.A);
     assert.notEqual(simpleEnum.A, simpleEnum.B);
 });
 
 QUnit.test('iterating over enum yields the names of values', function(assert) {
-    var simpleEnum = new Enum({A: 0, B: 1});
+    var simpleEnum = new Enum("A", "B");;
 
     var keys = [];
     for (var key in simpleEnum)
@@ -27,13 +27,14 @@ QUnit.test('iterating over enum yields the names of values', function(assert) {
 });
 
 QUnit.test('enum values print readably', function(assert) {
-    var simpleEnum = new Enum({A: 0, B: 1 });
+    var simpleEnum = new Enum("A", "B");;
     assert.equal(simpleEnum.B.toString(), "B");
 });
 
-QUnit.test('enum values can be mapped to aritrary integers', function(assert) {
-    var simpleEnum = new Enum({A: 0, B: 42 });
-    assert.equal(simpleEnum.B.index, 42);
+QUnit.test('enum values have consecutive indexes', function(assert) {
+    var simpleEnum = new Enum("A", "B");;
+    assert.equal(simpleEnum.A.index, 0);
+    assert.equal(simpleEnum.B.index, 1);
 });
 
 })();
