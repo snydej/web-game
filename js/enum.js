9e8fab5e16fc8e5085c68a50dd261a4655380652
Josh Snyder <joshsnyder@msn.com>
2015-10-13 01:54:09 -0700

    Remove the cyclical reference in enum typing and rename some private types to start with an underscore.

--- js/enum.js
+++ js/enum.js
@@ -1,43 +1,44 @@
 'use strict';
 
-function EnumValue(name, index, enumType) {
+function _EnumValue(name, index, typeRef) {
     this.name = name;
     this.index = index;
-    this.enumType = enumType;
+    this._typeRef = typeRef;
 }
 
-EnumValue.prototype.toString = function() {
+_EnumValue.prototype.toString = function() {
     return this.name;
 };
 
 function Enum() {
     this.values = [];
+    this._typeRef = {};
 
     var numValues = arguments.length;
     for (var i = 0; i < numValues; i++) {
-        var value = new EnumValue(arguments[i], i, this);
+        var value = new _EnumValue(arguments[i], i, this._typeRef);
         this[arguments[i]] = value;
         this.values.push(value);
     }
 }
 
 Enum.prototype.typeCheck = function(enumValue) {
-    if (enumValue.enumType !== this)
+    if (enumValue._typeRef !== this._typeRef)
         throw 'value is not from this enum';
 }
 
-function EnumMap(enumType, map) {
+function _EnumMap(enumType, map) {
     this.enumType = enumType;
     this.table = [];
     for (var key in map)
         this.table[enumType[key].index] = map[key];
 }
 
-EnumMap.prototype.get = function(enumValue) {
+_EnumMap.prototype.get = function(enumValue) {
     this.enumType.typeCheck(enumValue);
     return this.table[enumValue.index];
 };
 
 Enum.prototype.map = function(map) {
-    return new EnumMap(this, map);
+    return new _EnumMap(this, map);
 }
