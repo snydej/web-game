d32efbca4a3d041a1846a6fedcdd1b0c6a5db76f
Josh Snyder <joshsnyder@msn.com>
2015-10-12 23:29:38 -0700

    Add a map that uses enums as keys.

--- js/enum.js
+++ js/enum.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function EnumValue(name, index, enumType) {
     this.name = name;
@@ -8,10 +8,29 @@ function EnumValue(name, index, enumType) {
 
 EnumValue.prototype.toString = function() {
     return this.name;
-}
+};
 
 function Enum() {
+    this.values = [];
+
     var numValues = arguments.length;
-    for (var i = 0; i < numValues; i++)
-        this[arguments[i]] = new EnumValue(arguments[i], i, this);
+    for (var i = 0; i < numValues; i++) {
+        var value = new EnumValue(arguments[i], i, this);
+        this[arguments[i]] = value;
+        this.values.push(value);
+    }
+}
+
+function EnumMap(enumType, map) {
+    this.table = [];
+    for (var key in map)
+        this.table[enumType[key].index] = map[key];
+}
+
+EnumMap.prototype.get = function(enumValue) {
+    return this.table[enumValue.index];
+};
+
+Enum.prototype.map = function(map) {
+    return new EnumMap(this, map);
 }
--- test/js/enum-test.js
+++ test/js/enum-test.js
@@ -4,37 +4,40 @@
 QUnit.module('enum');
 
 QUnit.test('enum has specified elements', function(assert) {
-    var simpleEnum = new Enum("A", "B");;
+    var simpleEnum = new Enum('A', 'B');;
     assert.ok(simpleEnum.A);
     assert.ok(simpleEnum.B);
-    assert.notOk(simpleEnum.C);
+    assert.equal(simpleEnum.C, undefined);
 });
 
 QUnit.test('enum values are distict', function(assert) {
-    var simpleEnum = new Enum("A", "B");;
+    var simpleEnum = new Enum('A', 'B');;
     assert.equal(simpleEnum.A, simpleEnum.A);
     assert.notEqual(simpleEnum.A, simpleEnum.B);
 });
 
-QUnit.test('iterating over enum yields the names of values', function(assert) {
-    var simpleEnum = new Enum("A", "B");;
-
-    var keys = [];
-    for (var key in simpleEnum)
-        keys.push(key);
-
-    assert.deepEqual(keys, ["A", "B"]);
+QUnit.test('values contain all the enum values', function(assert) {
+    var simpleEnum = new Enum('A', 'B');;
+    assert.deepEqual(simpleEnum.values, [simpleEnum.A, simpleEnum.B]);
 });
 
 QUnit.test('enum values print readably', function(assert) {
-    var simpleEnum = new Enum("A", "B");;
-    assert.equal(simpleEnum.B.toString(), "B");
+    var simpleEnum = new Enum('A', 'B');;
+    assert.equal(simpleEnum.B.toString(), 'B');
 });
 
 QUnit.test('enum values have consecutive indexes', function(assert) {
-    var simpleEnum = new Enum("A", "B");;
+    var simpleEnum = new Enum('A', 'B');;
     assert.equal(simpleEnum.A.index, 0);
     assert.equal(simpleEnum.B.index, 1);
+});
+
+QUnit.test('enum maps map enum elements to values', function (assert) {
+    var simpleEnum = new Enum('A', 'B');
+    var map = simpleEnum.map({ A: 42, B: 'Hello'});
+
+    assert.equal(map.get(simpleEnum.A), 42);
+    assert.equal(map.get(simpleEnum.B), 'Hello');
 });
 
 })();
